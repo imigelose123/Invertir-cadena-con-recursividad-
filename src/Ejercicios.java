b8cd3aa5275fae5e43ae929b5d8c76a1de70571e
Jose Miguel Ascuy Anturiano <imigelose2@gmail.com>
2022-06-27 10:40:53 -0400

    ejercicio 2

---
+++ src/Ejercicios.java
@@ -0,0 +1,18 @@
+public class Ejercicios {
+
+    private static String invertirCadena(String cadena, int indice) {
+        String inversa;
+
+        if (indice == 0) {
+            inversa = String.valueOf(cadena.charAt(indice));
+        } else {
+            inversa = String.valueOf(cadena.charAt(indice)) + invertirCadena(cadena, indice - 1);
+        }
+
+        return inversa;
+    }
+
+    public static String invertirCadena(String cadena) {
+       return invertirCadena(cadena, cadena.length() -1);
+    }
+}
